de9802d9568b954aa2c72e4f2deebaf7b21953c5
Edinson Cotrina <developerwin-04@win.pe>
2025-03-10 23:56:22 -0500

    Initial commit

---
+++ src/main/java/com/yawarSoft/YawarSoftApplication.java
@@ -0,0 +1,13 @@
+package com.yawarSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YawarSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YawarSoftApplication.class, args);
+	}
+
+}
